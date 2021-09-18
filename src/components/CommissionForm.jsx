bbac1ee957f154c29aa73cdcd2131162417cba0c
n-rdy <giuseppegelardi@icloud.com>
2021-09-18 14:17:43 -0500

    wrapped commissions form in a div and added an onscreen representation of the chosen options

--- src/components/CommissionForm.jsx
+++ src/components/CommissionForm.jsx
@@ -51,33 +51,42 @@ const CommissionForm = (props) => {
   }
   
   return (
-    <Form onSubmit={submit}>
-        <Form.Select aria-label="layout"
-        onChange={e => setLayout(e.currentTarget.value)}>
-          <option>Select Layout</option>
-          <option key="1" value="60%">60%</option>
-          <option key="2" value="alice">Alice</option>
-          <option key="3" value="TKL">TKL</option>
-        </Form.Select>
+    <div className="commissionform">
+      <Form onSubmit={submit}>
+          <Form.Select aria-label="layout"
+          onChange={e => setLayout(e.currentTarget.value)}>
+            <option>Select Layout</option>
+            <option key="1" value="60%">60%</option>
+            <option key="2" value="alice">Alice</option>
+            <option key="3" value="TKL">TKL</option>
+          </Form.Select>
 
-        <Form.Select aria-label="color"
-        onChange={e => setColor(e.currentTarget.value)}>
-          <option>Keyboard Color</option>
-          <option key="1" value="obsidian">Obsidian</option>
-          <option key="2" value="ewhite">E-White</option>
-          <option key="3" value="polycarb">Polycarbonate</option>
-        </Form.Select>
+          <Form.Select aria-label="color"
+          onChange={e => setColor(e.currentTarget.value)}>
+            <option>Keyboard Color</option>
+            <option key="1" value="obsidian">Obsidian</option>
+            <option key="2" value="ewhite">E-White</option>
+            <option key="3" value="polycarb">Polycarbonate</option>
+          </Form.Select>
 
-        <Form.Select aria-label="keycap-color"
-        onChange={e => setKeycap_color(e.currentTarget.value)}>
-          <option>Keycap Color!</option>
-          <option key="1" value="dracula">Dracula</option>
-          <option key="2" value="muted">Muted</option>
-          <option key="3" value="futurefunk">Future Funk</option>
-        </Form.Select>
+          <Form.Select aria-label="keycap-color"
+          onChange={e => setKeycap_color(e.currentTarget.value)}>
+            <option>Keycap Color!</option>
+            <option key="1" value="dracula">Dracula</option>
+            <option key="2" value="muted">Muted</option>
+            <option key="3" value="futurefunk">Future Funk</option>
+          </Form.Select>
 
-        <Button type="submit">Submit!</Button>
-      </Form>
+          <Button type="submit">Submit!</Button>
+        </Form>
+
+        <br />
+
+        <p>You chose a {layout}</p>
+        <p>in {color}</p>
+        <p>with {keycap_color} keycaps!</p>
+      </div>
+
   )
 }
 
