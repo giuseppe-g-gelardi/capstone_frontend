66e9211efc4be3272b74aa7ca8703429f602b278
n-rdy <giuseppegelardi@icloud.com>
2021-09-20 18:13:35 -0500

    started filter function, works on one parameter

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 
 const Inventory = () => {
   const [inventoryData, setInventoryData] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
 
   useEffect(() => {
     getInventory()
@@ -34,8 +35,74 @@ const Inventory = () => {
 
     <div>
       <h2>Current inventory</h2> 
+      <button onClick={getData}>log the datas!!</button>
+
+
+      <div className='inventory'>
+        <input
+          type='text'
+          placeholder='Filter...'
+          onChange={e => {
+            setSearchTerm(e.target.value)
+          }}
+          />
+          {inventoryData.filter((val) => {
+            if (searchTerm === '') {
+              return val
+            } else if 
+            (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+              return val
+            }
+          }).map((val, key) => {
+            return (
+              <div className='table' key={key}>
+
+                <Table responsive striped bordered hover variant="dark" >
+                  <thead >
+                    <tr>
+                      <th>Name</th>
+                      <th>Layout</th>
+                      <th>Color</th>
+                      <th>Angle</th>
+                      <th>Switches</th>
+                      <th>Keycaps</th>
+                      <th>Plate</th>
+                      <th>Price</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>{val.name}</td>
+                      <td>{val.layout}</td>
+                      <td>{val.color}</td>
+                      <td>{val.angle}</td>
+                      <td>{val.switches}</td>
+                      <td>{val.keycaps}</td>
+                      <td>{val.plate}</td>
+                      <td>{val.price}</td>
+                    </tr>
+                  </tbody>
+
+                </Table>
+
+              </div>
+            )
+          })
+          }
+
+
+      </div>
+
+     
+
+
+
+
+
+
+
       
-      <Table responsive striped bordered hover variant="dark" >
+      {/* <Table responsive striped bordered hover variant="dark" >
         <thead >
           <tr>
             <th>Name</th>
@@ -64,10 +131,84 @@ const Inventory = () => {
         </tbody>
       </Table>
 
-      <button onClick={getData}>log the datas!!</button>
-      
+       */}
     </div>
   )
 }
 
 export default Inventory
+
+
+// import React, { useState, useEffect } from 'react';
+// import { Table } from 'react-bootstrap';
+// import axios from 'axios';
+// // import { TiFilter } from "react-icons/ti";
+
+// const Inventory = () => {
+//   const [inventoryData, setInventoryData] = useState([])
+
+//   useEffect(() => {
+//     getInventory()
+//   }, [])
+
+//   const getInventory = async () => {
+//     try {
+//       axios.get('http://127.0.0.1:8000/api/inventory/all/').then((response) => {
+//         setInventoryData(response.data)
+//       },(err) => {
+//         console.log(err)
+//       })
+//     }
+//     catch(except) {
+//       console.log(except)
+//     }
+//   }
+
+
+//   const getData = () => {
+//     console.log(inventoryData)
+//   }
+
+  
+
+//   return (
+
+//     <div>
+//       <h2>Current inventory</h2> 
+      
+//       <Table responsive striped bordered hover variant="dark" >
+//         <thead >
+//           <tr>
+//             <th>Name</th>
+//             <th>Layout</th>
+//             <th>Color</th>
+//             <th>Angle</th>
+//             <th>Switches</th>
+//             <th>Keycaps</th>
+//             <th>Plate</th>
+//             <th>Price</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {inventoryData.map((item) => 
+//           <tr>
+//             <td>{item.name}</td>
+//             <td>{item.layout}</td>
+//             <td>{item.color}</td>
+//             <td>{item.angle}</td>
+//             <td>{item.switches}</td>
+//             <td>{item.keycaps}</td>
+//             <td>{item.plate}</td>
+//             <td>{item.price}</td>
+//           </tr>
+//           )}
+//         </tbody>
+//       </Table>
+
+//       <button onClick={getData}>log the datas!!</button>
+      
+//     </div>
+//   )
+// }
+
+// export default Inventory
