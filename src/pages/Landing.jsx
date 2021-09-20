7bf12ac539a83120dc1cea7429c2debdf0745447
n-rdy <giuseppegelardi@icloud.com>
2021-09-20 14:21:32 -0500

    added some styles to landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,13 +2,11 @@ import React, { useState } from 'react';
 import { Redirect } from 'react-router';
 // import landing from '../images/landing.png';
 import midway from '../images/midway.jpg'
-import { Image, Container, Card, Button  } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 
 const Landing = () => {
   const [redirect, setRedirect] = useState(false)
 
-
-  
   if (redirect) {
     return <Redirect to='/home'/>
   }
@@ -18,11 +16,12 @@ const Landing = () => {
     <Card className="bg-dark text-white">
     <Card.Img src={midway} />
     <Card.ImgOverlay>
-      <Card.Title>Elevate your workspace...</Card.Title>
-      <Card.Text>
-        A custom bespoke mechanical keyboard will help your fingers avoid fatigue during prolonged typing sessions.
+      <Card.Title id="landing-title">Elevate your workspace...</Card.Title>
+      <Card.Text id="landing-text">
+        A custom bespoke mechanical keyboard will help your fingers avoid fatigue during prolonged typing sessions. 
       </Card.Text>
-      <Button onClick={e => setRedirect(true)}>Enter</Button>
+      <Button id="enter-button" onClick={e => setRedirect(true)}>Enter</Button>
+      
     </Card.ImgOverlay>
   </Card>
     
