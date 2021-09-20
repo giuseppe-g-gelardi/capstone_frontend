87d8249e43c79d08d47b6c4a5ec9353038e2bef5
n-rdy <giuseppegelardi@icloud.com>
2021-09-20 18:45:06 -0500

    filter function works

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { Table } from 'react-bootstrap';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import { Table } from 'react-bootstrap'
+import axios from 'axios'
 // import { TiFilter } from "react-icons/ti";
 
 const Inventory = () => {
@@ -13,30 +13,22 @@ const Inventory = () => {
 
   const getInventory = async () => {
     try {
-      axios.get('http://127.0.0.1:8000/api/inventory/all/').then((response) => {
-        setInventoryData(response.data)
-      },(err) => {
-        console.log(err)
-      })
-    }
-    catch(except) {
+      axios.get('http://127.0.0.1:8000/api/inventory/all/').then(
+        response => {
+          setInventoryData(response.data)
+        },
+        err => {
+          console.log(err)
+        }
+      )
+    } catch (except) {
       console.log(except)
     }
   }
 
-
-  const getData = () => {
-    console.log(inventoryData)
-  }
-
-  
-
   return (
-
     <div>
-      <h2>Current inventory</h2> 
-      <button onClick={getData}>log the datas!!</button>
-
+      <h2>Current inventory</h2>
 
       <div className='inventory'>
         <input
@@ -45,33 +37,41 @@ const Inventory = () => {
           onChange={e => {
             setSearchTerm(e.target.value)
           }}
-          />
-          {inventoryData.filter((val) => {
-            if (searchTerm === '') {
-              return val
-            } else if 
-            (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
-              return val
-            }
-          }).map((val, key) => {
-            return (
-              <div className='table' key={key}>
-
-                <Table responsive striped bordered hover variant="dark" >
-                  <thead >
-                    <tr>
-                      <th>Name</th>
-                      <th>Layout</th>
-                      <th>Color</th>
-                      <th>Angle</th>
-                      <th>Switches</th>
-                      <th>Keycaps</th>
-                      <th>Plate</th>
-                      <th>Price</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
+        />
+        <div className='table'>
+          <Table responsive striped bordered hover variant='dark'>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Layout</th>
+                <th>Color</th>
+                <th>Angle</th>
+                <th>Switches</th>
+                <th>Keycaps</th>
+                <th>Plate</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {inventoryData
+                .filter(val => {
+                  let searchString = ''
+                  for (let [key, value] of Object.entries(val)) {
+                    searchString += `${value}\t`
+                  }
+                  if (searchTerm === '') {
+                    return val
+                  } else if (
+                    searchString
+                      .toLowerCase()
+                      .includes(searchTerm.toLowerCase())
+                  ) {
+                    return val
+                  }
+                })
+                .map((val, key) => {
+                  return (
+                    <tr key={key}>
                       <td>{val.name}</td>
                       <td>{val.layout}</td>
                       <td>{val.color}</td>
@@ -81,134 +81,14 @@ const Inventory = () => {
                       <td>{val.plate}</td>
                       <td>{val.price}</td>
                     </tr>
-                  </tbody>
-
-                </Table>
-
-              </div>
-            )
-          })
-          }
-
-
+                  )
+                })}
+            </tbody>
+          </Table>
+        </div>
       </div>
-
-     
-
-
-
-
-
-
-
-      
-      {/* <Table responsive striped bordered hover variant="dark" >
-        <thead >
-          <tr>
-            <th>Name</th>
-            <th>Layout</th>
-            <th>Color</th>
-            <th>Angle</th>
-            <th>Switches</th>
-            <th>Keycaps</th>
-            <th>Plate</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {inventoryData.map((item) => 
-          <tr>
-            <td>{item.name}</td>
-            <td>{item.layout}</td>
-            <td>{item.color}</td>
-            <td>{item.angle}</td>
-            <td>{item.switches}</td>
-            <td>{item.keycaps}</td>
-            <td>{item.plate}</td>
-            <td>{item.price}</td>
-          </tr>
-          )}
-        </tbody>
-      </Table>
-
-       */}
     </div>
   )
 }
 
 export default Inventory
-
-
-// import React, { useState, useEffect } from 'react';
-// import { Table } from 'react-bootstrap';
-// import axios from 'axios';
-// // import { TiFilter } from "react-icons/ti";
-
-// const Inventory = () => {
-//   const [inventoryData, setInventoryData] = useState([])
-
-//   useEffect(() => {
-//     getInventory()
-//   }, [])
-
-//   const getInventory = async () => {
-//     try {
-//       axios.get('http://127.0.0.1:8000/api/inventory/all/').then((response) => {
-//         setInventoryData(response.data)
-//       },(err) => {
-//         console.log(err)
-//       })
-//     }
-//     catch(except) {
-//       console.log(except)
-//     }
-//   }
-
-
-//   const getData = () => {
-//     console.log(inventoryData)
-//   }
-
-  
-
-//   return (
-
-//     <div>
-//       <h2>Current inventory</h2> 
-      
-//       <Table responsive striped bordered hover variant="dark" >
-//         <thead >
-//           <tr>
-//             <th>Name</th>
-//             <th>Layout</th>
-//             <th>Color</th>
-//             <th>Angle</th>
-//             <th>Switches</th>
-//             <th>Keycaps</th>
-//             <th>Plate</th>
-//             <th>Price</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {inventoryData.map((item) => 
-//           <tr>
-//             <td>{item.name}</td>
-//             <td>{item.layout}</td>
-//             <td>{item.color}</td>
-//             <td>{item.angle}</td>
-//             <td>{item.switches}</td>
-//             <td>{item.keycaps}</td>
-//             <td>{item.plate}</td>
-//             <td>{item.price}</td>
-//           </tr>
-//           )}
-//         </tbody>
-//       </Table>
-
-//       <button onClick={getData}>log the datas!!</button>
-      
-//     </div>
-//   )
-// }
-
-// export default Inventory
