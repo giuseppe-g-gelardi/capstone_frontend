1787bba5d7f334509dbd9ad25ab120a1a36de711
n-rdy <giuseppegelardi@icloud.com>
2021-09-20 20:27:04 -0500

    removed unused import

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Table } from 'react-bootstrap'
 import axios from 'axios'
-// import { TiFilter } from "react-icons/ti";
 
 const Inventory = () => {
   const [inventoryData, setInventoryData] = useState([])
@@ -92,3 +91,101 @@ const Inventory = () => {
 }
 
 export default Inventory
+
+
+
+
+// import React, { useState, useEffect } from 'react'
+// import { Table } from 'react-bootstrap'
+// import axios from 'axios'
+// // import { TiFilter } from "react-icons/ti";
+
+// const Inventory = () => {
+//   const [inventoryData, setInventoryData] = useState([])
+//   const [searchTerm, setSearchTerm] = useState('')
+
+//   useEffect(() => {
+//     getInventory()
+//   }, [])
+
+//   const getInventory = async () => {
+//     try {
+//       axios.get('http://127.0.0.1:8000/api/inventory/all/').then(
+//         response => {
+//           setInventoryData(response.data)
+//         },
+//         err => {
+//           console.log(err)
+//         }
+//       )
+//     } catch (except) {
+//       console.log(except)
+//     }
+//   }
+
+//   return (
+//     <div>
+//       <h2>Current inventory</h2>
+
+//       <div className='inventory'>
+//         <input
+//           type='text'
+//           placeholder='Filter...'
+//           onChange={e => {
+//             setSearchTerm(e.target.value)
+//           }}
+//         />
+//         <div className='table'>
+//           <Table responsive striped bordered hover variant='dark'>
+//             <thead>
+//               <tr>
+//                 <th>Name</th>
+//                 <th>Layout</th>
+//                 <th>Color</th>
+//                 <th>Angle</th>
+//                 <th>Switches</th>
+//                 <th>Keycaps</th>
+//                 <th>Plate</th>
+//                 <th>Price</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {inventoryData
+//                 .filter(val => {
+//                   let searchString = ''
+//                   for (let [key, value] of Object.entries(val)) {
+//                     searchString += `${value}\t`
+//                   }
+//                   if (searchTerm === '') {
+//                     return val
+//                   } else if (
+//                     searchString
+//                       .toLowerCase()
+//                       .includes(searchTerm.toLowerCase())
+//                   ) {
+//                     return val
+//                   }
+//                 })
+//                 .map((val, key) => {
+//                   return (
+//                     <tr key={key}>
+//                       <td>{val.name}</td>
+//                       <td>{val.layout}</td>
+//                       <td>{val.color}</td>
+//                       <td>{val.angle}</td>
+//                       <td>{val.switches}</td>
+//                       <td>{val.keycaps}</td>
+//                       <td>{val.plate}</td>
+//                       <td>{val.price}</td>
+//                     </tr>
+//                   )
+//                 })}
+//             </tbody>
+//           </Table>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Inventory
