5a2a796e101b60194ca8bcfeb5c522aa2b80427e
n-rdy <giuseppegelardi@icloud.com>
2021-09-21 02:58:14 -0500

    added a displayed image of the chosen options for the keyboard builder

--- src/components/BuilderForm.jsx
+++ src/components/BuilderForm.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
-import axios from 'axios';
-import jwtDecode from 'jwt-decode';
-import BuilderSubmit from './BuilderSubmit';
+import React, { useState } from 'react'
+import { Form, Button, Image } from 'react-bootstrap'
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
+import BuilderSubmit from './BuilderSubmit'
+import BuilderImage from './BuilderImage'
+
+
 
 const BuilderForm = () => {
   const [user, setUser] = useState({})
@@ -11,104 +14,145 @@ const BuilderForm = () => {
   const [keycap_color, setKeycap_color] = useState({})
   const [isSubmitted, setIsSubmitted] = useState(false)
 
-  const submit = async (e) => {
-      e.preventDefault();
-      
-      const token = localStorage.getItem('token');
-      const userdata = jwtDecode(token)
-      const userid = userdata['user_id']
-      setUser(userid)
-      // console.log(user)
+  const submit = async e => {
+    e.preventDefault()
 
-      const headers = {
-        'Authorization': `Bearer ${token}` 
-      }
+    const token = localStorage.getItem('token')
+    const userdata = jwtDecode(token)
+    const userid = userdata['user_id']
+    setUser(userid)
+    // console.log(user)
 
-      const data = {
-        'layout': layout,
-        'color': color,
-        'keycap_color': keycap_color,
-        'user': user,
-      }
+    const headers = {
+      Authorization: `Bearer ${token}`
+    }
 
-      const options = {
-        method: 'POST',
-        headers: headers,
-        data: data,
-        url: `http://127.0.0.1:8000/api/users/keyboards/`
-      }
+    const data = {
+      layout: layout,
+      color: color,
+      keycap_color: keycap_color,
+      user: user
+    }
 
-      try{
-        console.log(options)
-        axios(options).then((response) => {
+    const options = {
+      method: 'POST',
+      headers: headers,
+      data: data,
+      url: `http://127.0.0.1:8000/api/users/keyboards/`
+    }
+
+    try {
+      console.log(options)
+      axios(options).then(
+        response => {
           // console.log(response)
           setIsSubmitted(true)
-        }, (err) => {
+        },
+        err => {
           console.log(err)
-        })
-      } catch(except) {
-        console.log('im catching')
-      }
+        }
+      )
+    } catch (except) {
+      console.log('im catching')
+    }
   }
+
+  
+
+
 
   return (
 
 
     <div class="container">
-    <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-6">
+        <div class="row">
+            <div class="col-md-6">
+              <p>Welcome to the Keyboard Builder(beta)!</p>
+              <p>Here you can pick a layout, color and keycaps and submit them and it will display the keyboard with the options you chose. Have fun!</p>
+            </div>
 
 
 
-        <div>
-    <div className="builderform">
-      <Form onSubmit={submit}>
-          <Form.Select aria-label="layout"
-          onChange={e => setLayout(e.currentTarget.value)}>
-            <option>Select Layout</option>
-            <option key="1" value="60%">60%</option>
-            <option key="2" value="TKL">TKL</option>
-          </Form.Select>
+            <div class="col-md-6">
 
-          <Form.Select aria-label="color"
-          onChange={e => setColor(e.currentTarget.value)}>
-            <option>Keyboard Color</option>
-            <option key="1" value="obsidian">Obsidian</option>
-            <option key="2" value="ewhite">E-White</option>
-          </Form.Select>
+            <Form onSubmit={submit}>
+                <Form.Select
+                  aria-label='layout'
+                  onChange={e => setLayout(e.currentTarget.value)}
+                >
+                  <option>Select Layout</option>
+                  <option key='1' value='60%'>
+                    60%
+                  </option>
+                  <option key='2' value='TKL'>
+                    TKL
+                  </option>
+                </Form.Select>
 
-          <Form.Select aria-label="keycap-color"
-          onChange={e => setKeycap_color(e.currentTarget.value)}>
-            <option>Keycap Color!</option>
-            <option key="1" value="dracula">Dracula</option>
-            <option key="2" value="muted">Muted</option>
-          </Form.Select>
+                <Form.Select
+                  aria-label='color'
+                  onChange={e => setColor(e.currentTarget.value)}
+                >
+                  <option>Keyboard Color</option>
+                  <option key='1' value='obsidian'>
+                    Obsidian
+                  </option>
+                  <option key='2' value='ewhite'>
+                    E-White
+                  </option>
+                </Form.Select>
 
-          <Button type="submit">Submit!</Button>
-        </Form>
+                <Form.Select
+                  aria-label='keycap-color'
+                  onChange={e => setKeycap_color(e.currentTarget.value)}
+                >
+                  <option>Keycap Color!</option>
+                  <option key='1' value='dracula'>
+                    Dracula
+                  </option>
+                  <option key='2' value='muted'>
+                    Muted
+                  </option>
+                </Form.Select>
+
+                <Button type='submit'>Submit!</Button>
+              </Form>
+            </div>
+            <BuilderSubmit
+              submitted={isSubmitted}
+              layout={layout}
+              color={color}
+              keycaps={keycap_color}
+            />
+
+            <BuilderImage
+              submitted={isSubmitted}
+              layout={layout}
+              color={color}
+              keycaps={keycap_color}
+            />
+
+
+
+              
+            </div>
+
+
+
+
+
+            <div class="col-md-3"></div>
+            <div class="col-md-6"></div>
+            <div class="col-md-3"></div>
         </div>
-        <BuilderSubmit 
-          submitted={isSubmitted}
-          layout={layout}
-          color={color}
-          keycaps={keycap_color}
-        />
-      </div>
-
-
-
-        </div>
-        <div class="col-md-3"></div>
-    </div>
-    </div>
-
-
-
-
-
     
+          
+          
+        
   )
 }
 
 export default BuilderForm
+
+
+               
--- src/components/BuilderSubmit.jsx
+++ src/components/BuilderSubmit.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
+
 
 const BuilderSubmit = (props) => {
+
+  
+
 
   if (props.submitted === true) {
     return (
       <>
         <p>
-          you chose an {props.color} {props.layout} with {props.keycaps} keycaps!
-          </p>
+          you chose an {props.color} {props.layout} with {props.keycaps} keycaps! <br/>Press "Submit!" to save.
+        </p>
       </>
     )
   } else {
